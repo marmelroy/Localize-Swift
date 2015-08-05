ddb5c074b6990f0ecfbb8f13f240e5e84b636eb3
Roy Marmelstein <marmelroy@gmail.com>
2015-08-05 13:36:54 +0200

    Xcode settings and header update

--- Localize/Localize.h
+++ Localize/Localize.h
@@ -6,9 +6,7 @@
 //  Copyright © 2015 Roy Marmelstein. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-
-#undef NSLocalizedString
+#import <Foundation/Foundation.h>
 
 //! Project version number for Localize.
 FOUNDATION_EXPORT double LocalizeVersionNumber;
