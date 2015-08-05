d4b818852ec09de99f6e83ee2428b3081f3fc94d
Roy Marmelstein <marmelroy@gmail.com>
2015-08-05 10:53:55 +0200

    Initial Commit

---
+++ Localize/Localize.h
@@ -0,0 +1,19 @@
+//
+//  Localize.h
+//  Localize
+//
+//  Created by Roy Marmelstein on 05/08/2015.
+//  Copyright © 2015 Roy Marmelstein. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Localize.
+FOUNDATION_EXPORT double LocalizeVersionNumber;
+
+//! Project version string for Localize.
+FOUNDATION_EXPORT const unsigned char LocalizeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Localize/PublicHeader.h>
+
+
