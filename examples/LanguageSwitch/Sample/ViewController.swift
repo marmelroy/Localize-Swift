70ce7a891e35b88adef0ade79fc70babca86106a
Roy Marmelstein <marmelroy@gmail.com>
2015-08-05 21:00:15 +0200

    First example initial commit

---
+++ examples/LanguageSwitch/Sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sample
+//
+//  Created by Roy Marmelstein on 05/08/2015.
+//  Copyright (c) 2015 Roy Marmelstein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
