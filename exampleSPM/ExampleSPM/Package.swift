ee665c5337a26b13440ad92d2eb40773ad966304
Roy Marmelstein <marmelroy@gmail.com>
2015-12-07 22:30:14 +0100

    Remove SPM example

--- exampleSPM/ExampleSPM/Package.swift
+++
@@ -1,8 +0,0 @@
-import PackageDescription
-
-let package = Package(
-    name: "Localize",
-    dependencies: [
-        .Package(url: "https://github.com/marmelroy/Localize-Swift", versions: "0.6" ..< Version.max),
-    ]
-)
