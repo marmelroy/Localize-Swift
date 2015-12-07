3f3782ea0cf2b60f267b2f7d0ae7b507217f2973
Roy Marmelstein <marmelroy@gmail.com>
2015-12-07 22:18:41 +0100

    Updating package.swift for SPM example

--- exampleSPM/LanguageSwitch/Package.swift
+++ exampleSPM/LanguageSwitch/Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "Sample",
     dependencies: [
-        .Package(url: "https://github.com/marmelroy/Localize-Swift"),
+        .Package(url: "https://github.com/marmelroy/Localize-Swift", versions: "0.6" ..< Version.max),
     ]
 )
