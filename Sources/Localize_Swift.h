e4db22f98e12d84f3a1e7b0e8c510afc2a2e487b
Roy Marmelstein <marmelroy@gmail.com>
2016-01-21 22:58:07 +0100

    Xcode clean up

---
+++ Sources/Localize_Swift.h
@@ -0,0 +1,19 @@
+//
+//  Localize_Swift.h
+//  Localize_Swift
+//
+//  Created by Roy Marmelstein on 21/01/2016.
+//  Copyright © 2016 Roy Marmelstein. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Localize_Swift.
+FOUNDATION_EXPORT double Localize_SwiftVersionNumber;
+
+//! Project version string for Localize_Swift.
+FOUNDATION_EXPORT const unsigned char Localize_SwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Localize_Swift/PublicHeader.h>
+
+
