dfa9bd53c615552d289ac4391ba212483642df5f
Roy Marmelstein <marmelroy@gmail.com>
2016-08-27 11:49:13 +0300

    Fix travis

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # **** Update me when new Xcode versions are released! ****
-PLATFORM="platform=iOS Simulator,OS=9.3,name=iPhone 6s"
+PLATFORM="platform=iOS Simulator,OS=10.0,name=iPhone 6s"
 SDK="iphonesimulator"
 
 
