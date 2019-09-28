19123eef354205d52978daf60d86c92d1a9e5c50
Kristaps Grinbergs <fassko@gmail.com>
2019-09-28 17:12:09 +0300

    Fix SwiftPM build

--- Package.swift
+++ Package.swift
@@ -1,5 +1,19 @@
+// swift-tools-version:5.1
+
 import PackageDescription
 
 let package = Package(
-  name: "Localize_Swift"
+    name: "Localize_Swift",
+    products: [
+        .library(
+            name: "Localize_Swift",
+            targets: ["Localize_Swift"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "Localize_Swift",
+            path: "Sources")
+    ],
+    swiftLanguageVersions: [.v5]
 )
